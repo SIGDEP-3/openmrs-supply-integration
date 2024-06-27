b12d0db4861f1bcf2df2360bbaa63121652b2cba
Bogui <spbogui@gmail.com>
2024-06-27 11:28:28 +0000

    created connexion form class

---
+++ api/src/main/java/org/openmrs/module/supplyintegration/api/forms/ConnexionFormValidation.java
@@ -0,0 +1,26 @@
+package org.openmrs.module.supplyintegration.api.forms;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class ConnexionFormValidation implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return clazz.equals(ConnexionForm.class);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		ConnexionForm form = (ConnexionForm) target;
+		
+		if (form == null) {
+			errors.reject("integration", "general.error");
+		} else {
+			ValidationUtils.rejectIfEmpty(errors, "url", null, "Ce champ est requis");
+			ValidationUtils.rejectIfEmpty(errors, "username", null, "Ce champ est requis");
+			ValidationUtils.rejectIfEmpty(errors, "password", null, "Ce champ est requis");
+		}
+	}
+}
